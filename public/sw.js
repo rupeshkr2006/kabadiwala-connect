21ddad45388a50979a6039e97a7b1103754608bb
rupeshkr2006 <rupeshvarmakr2006@gmail.com>
2026-09-24 21:39:16 +0530

    Invalidate stale public PWA cache after admin rebuild

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "kabadiwala-connect-v7-admin";
+const CACHE_NAME = "kabadiwala-connect-v9-admin-final";
 const APP_SHELL = [
   "/", "/index.html", "/main.mjs", "/styles.css", "/favicon.svg", "/offline-db.mjs",
   "/manifest.webmanifest", "/models/labels.json", "/models/ewaste_mobilenetv2_fp16.tflite",
